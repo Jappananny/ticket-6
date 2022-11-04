5c43b9f1e253ae95b2acaa07c09ec34c96abc26e
Jappananny <Jappananny@gmail.com>
2022-11-04 17:29:41 +0300

    Билет № 6 -  ИСПРАВЛЕНИЯ

--- src/Calculation.java
+++ src/Calculation.java
@@ -5,7 +5,7 @@ public class Calculation {
         for(int i = 0; i < number.length; i++){
             if(number[i] % 2 == 1){
                 number[i] += 1;
-                if(number[i]> 3 && number[i] < 7){
+                if(i > 3 && i < 7){
                     System.out.println(number[i] + " Искомое число");
                 }
             }
